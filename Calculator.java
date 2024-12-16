2f511b92cbc8ea203942a3dcf8a7bd9f08694496
Angel Galindo <ajga203@gmail.com>
2024-12-15 22:32:48 -0400

    first commit

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+//Calculator Project to print out certain values//
+
+public class Calculator{
+
+    public Calculator(){
+    }
+
+      public int add(int a, int b) {
+     
+     return a + b;
+    }
+
+       public int subtract(int a, int b) {
+        return a - b;
+    }
+
+       public int multiply(int a, int b) {
+        return a * b;
+    }
+
+      public int divide(int a, int b) {
+        return a / b;
+    }
+      
+      public int modulo(int a, int b){
+
+    return a % b;
+    }
+
+}
+
---
+++ main.java
@@ -0,0 +1,12 @@
+
+//Main class, prints out the results//
+
+public class main {
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(45, 11));
+        System.out.println(myCalculator.multiply(12, 34));
+    }
+}
